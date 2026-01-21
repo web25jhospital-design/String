eff27319b8d4a9caaa2b94e6a704cf191276f2f4
web25aperez <web25aperez@imaltuna.com>
2026-01-21 12:42:46 +0100

    CSV file-ak deskargatu eta data fitxategian sartu. 4 java aplikazioa sortu csv file-ak irakurtzeko.

--- Erronka/src/makin_irakurri.java
+++ Erronka/src/makin_irakurri.java
@@ -1,18 +1,18 @@
 import java.io.BufferedReader;
 import java.io.FileReader;
 
-public class erab_irakurri {
+public class makin_irakurri {
 public static void main(String[] args) {
 
         try {
-            BufferedReader br = new BufferedReader(new FileReader("data/erabiltzaileak.csv"));
+            BufferedReader br = new BufferedReader(new FileReader("data/makinak.csv"));
             String lerroa;
 
             br.readLine(); // hau salto egiteko lehengo lerroa
             while ((lerroa = br.readLine()) != null) {
                 String[] datuak = lerroa.split(",");
 
-                System.out.println(datuak[0]+"-"+datuak[1]+"-"+datuak[2]+"-"+datuak[3]+"-"+datuak[4]+"-"+datuak[5]+"-"+datuak[6]+"-"+datuak[7]+"-"+datuak[8]);
+                System.out.println(datuak[0]+"-"+datuak[1]+"-"+datuak[2]+"-"+datuak[3]+"-"+datuak[4]);
 
             
             }
