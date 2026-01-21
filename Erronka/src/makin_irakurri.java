8371791a9131cb7eaa80c4246254ddc5502ade26
web25aperez <web25aperez@imaltuna.com>
2026-01-21 13:33:06 +0100

    Irakurtzeko java aplikazioei Shft+Alt+F

--- Erronka/src/makin_irakurri.java
+++ Erronka/src/makin_irakurri.java
@@ -2,7 +2,7 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 
 public class makin_irakurri {
-public static void main(String[] args) {
+    public static void main(String[] args) {
 
         try {
             BufferedReader br = new BufferedReader(new FileReader("data/makinak.csv"));
@@ -12,9 +12,8 @@ public static void main(String[] args) {
             while ((lerroa = br.readLine()) != null) {
                 String[] datuak = lerroa.split(",");
 
-                System.out.println(datuak[0]+"-"+datuak[1]+"-"+datuak[2]+"-"+datuak[3]+"-"+datuak[4]);
+                System.out.println(datuak[0] + "-" + datuak[1] + "-" + datuak[2] + "-" + datuak[3] + "-" + datuak[4]);
 
-            
             }
             br.close();
         } catch (Exception e) {
