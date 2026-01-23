39a459bd1174d7acf5e6f7126a0a5fdda78cd943
web25aperez <web25aperez@imaltuna.com>
2026-01-23 09:53:12 +0100

    loturen csv fitxategiak sartu eta honen java irakurleak eta pieza.csv fitxategiari stock-a eta prezioa gehitu

--- Erronka/src/pieza_piezamota_irakurri.java
+++ Erronka/src/pieza_piezamota_irakurri.java
@@ -1,18 +1,18 @@
 import java.io.BufferedReader;
 import java.io.FileReader;
 
-public class pieza_irakurri {
-    public static void main(String[] args) {
+public class pieza_piezamota_irakurri {
+        public static void main(String[] args) {
 
         try {
-            BufferedReader br = new BufferedReader(new FileReader("data/piezak.csv"));
+            BufferedReader br = new BufferedReader(new FileReader("data/erabiltzaileak.csv"));
             String lerroa;
 
             br.readLine(); // hau salto egiteko lehengo lerroa
             while ((lerroa = br.readLine()) != null) {
                 String[] datuak = lerroa.split(",");
 
-                System.out.println(datuak[0] + "-" + datuak[1] + "-" + datuak[2] + "-" + datuak[3] + "-" + datuak[4] + "-" + datuak[5]);
+                System.out.println(datuak[0] + "-" + datuak[1]);
 
             }
             br.close();
@@ -20,5 +20,4 @@ public class pieza_irakurri {
             System.out.println("Errorea fitxategia irakurtzean");
         }
     }
-
 }
